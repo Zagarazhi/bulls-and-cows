05464c7ce09704756e3e5767db2b3b00f2d49fbc
Zagarazhi <fukuroOwl@yandex.ru>
2022-06-08 22:54:47 +0400

    update repositories

--- src/main/java/ru/zagarazhi/repositories/GameRepository.java
+++ src/main/java/ru/zagarazhi/repositories/GameRepository.java
@@ -1,5 +1,7 @@
 package ru.zagarazhi.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import ru.zagarazhi.entities.Game;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long>{
-    
+    Page<Game> findAllByUser_id(Long user_id, Pageable pageable);
 }
--- src/main/java/ru/zagarazhi/repositories/UserRepository.java
+++ src/main/java/ru/zagarazhi/repositories/UserRepository.java
@@ -1,5 +1,9 @@
 package ru.zagarazhi.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,6 @@ import ru.zagarazhi.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    
+    Optional<User> findByUserName(String username);
+    Page<User> findAllOrderByRating(Pageable pageable);
 }
