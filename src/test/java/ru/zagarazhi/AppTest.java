21228632718609a6cbcb53527e35dc69c8ee4be4
Zagarazhi <fukuroOwl@yandex.ru>
2022-06-08 14:30:40 +0400

    update autogenerated test to JUnit 5

--- src/test/java/ru/zagarazhi/AppTest.java
+++ src/test/java/ru/zagarazhi/AppTest.java
@@ -1,17 +1,9 @@
 package ru.zagarazhi;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
     @Test
     public void shouldAnswerWithTrue()
     {
