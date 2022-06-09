073cc52cace2c71d1fcc2b845f0b415934a1dc8d
Zagarazhi <fukuroOwl@yandex.ru>
2022-06-09 20:24:57 +0400

    add dto for registration

---
+++ src/main/java/ru/zagarazhi/entities/dto/UserRegistrationDto.java
@@ -0,0 +1,18 @@
+package ru.zagarazhi.entities.dto;
+
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+import ru.zagarazhi.constraints.FieldMatch;
+
+@Data
+@FieldMatch(first = "password", second = "passwordConfirm", message = "Пароли не совпадают")
+public class UserRegistrationDto {
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String passwordConfirm;
+}
